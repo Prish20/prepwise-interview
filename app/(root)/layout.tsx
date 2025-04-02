963780ac7a48187808860538967853347f9838f6
Prish20 <contact@christadriansanya.me>
2025-04-03 02:52:24 +0300

    Add InterviewCard component and DisplayTechIcons; update layout and add dayjs dependency

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className='flex gap-2 items-center'>
+                    <Image src="/logo.svg" alt="logo" width={32} height={38} />
+                    <h2 className="text-primary-100">Prepwise</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
     )
 }
 
